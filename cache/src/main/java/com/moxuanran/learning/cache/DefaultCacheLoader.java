78bcb66601cc63c6d617895ec97f210e3c24cb11
吴涛 <wutao@accesscorporate.com.cn>
2022-10-14 18:12:05 +0800

    stash

---
+++ cache/src/main/java/com/moxuanran/learning/cache/DefaultCacheLoader.java
@@ -0,0 +1,12 @@
+package com.moxuanran.learning.cache;
+
+/**
+ * @author wutao
+ * @date 2022/10/14 18:09
+ */
+public class DefaultCacheLoader implements ICacheLoader<Object,Object>{
+    @Override
+    public Object load(Object key) {
+        return null;
+    }
+}
